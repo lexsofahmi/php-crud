f4c73ced6425a84a095e9db216a5e9d10cfbf36c
lexsofahmi@gmail.com <lexsofahmi@gmail.com>
2015-04-27 17:00:01 +0700

    updated index.php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <?php
-    include 'module/header.php';
-    ?>
+<head><?php include 'module/header.php'; ?>
 </head>
 <body>
 <div class="container">
     <?php include 'module/nav.php'; ?>
 </div>
+<?php include 'module/footer.php'; ?>
 </body>
 </html>
