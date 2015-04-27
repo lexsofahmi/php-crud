a4ee58fe839dc52f22776b1aa0f4a2f2fcf35065
lexsofahmi@gmail.com <lexsofahmi@gmail.com>
2015-04-27 17:01:56 +0700

    updated header.php

--- module/header.php
+++ module/header.php
@@ -1,8 +1,12 @@
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>PHP CRUD</title>
+<meta name="generator" content="Bootply"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 <!-- Load Bootstrap CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<link rel="stylesheet" href="../assets/css/style.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
