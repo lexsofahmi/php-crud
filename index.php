711e00c4bd623380374526b1edb8001b06b5336a
lexsofahmi@gmail.com <lexsofahmi@gmail.com>
2015-04-28 09:06:51 +0700

    create input.php

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <!doctype html>
 <html lang="en">
-<head><?php include 'module/header.php'; ?>
+<head><?php
+    include 'module/header.php';
+    //include 'module/alerts.php';
+    include "config/connect.php";
+    ?>
 </head>
 <body>
 <div class="container">
     <?php include 'module/nav.php'; ?>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="page-header">
+                <h1>Form Input (Create)</h1>
+            </div>
+        </div>
+    </div>
+
+    <!--form -->
+    <?php include 'module/input.php'; ?>
 </div>
 <?php include 'module/footer.php'; ?>
 </body>
---
+++ module/input.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <form>
+            <div class="form-group">
+                <label class="control-label" for="nama">Nama (wajib diisi)</label>
+                <input type="text" class="form-control" name="nama" id="nama" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Simpan" name="simpan" class="btn btn-primary">
+                <input type="reset" value="Reset" class="btn btn-danger">
+            </div>
+        </form>
+    </div>
+</div>
